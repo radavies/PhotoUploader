d3d94727ea86e2dee973205ca7e7f7843231dfb4
Raymond Davies <rad@radavies.net>
2026-06-18 15:32:44 +0100

    Update misc.py
    new season upload path

--- misc.py
+++ misc.py
@@ -8,4 +8,4 @@ class Misc(Enum):
     LogFolderPath = "logs"
     LogFileName = "log.txt"
     DropboxCredsFileName = "dropbox_creds.json"
-    DropboxUploadPath = '/SEASON 2025-26/8. Player Pictures'
+    DropboxUploadPath = '/SEASON 2026-27/3. Player Pictures'
